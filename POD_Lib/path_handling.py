27166658ce6ac48897e7ffada40a72b730904fa7
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-10-24 15:41:51 +0700

    add path handling

---
+++ POD_Lib/path_handling.py
@@ -0,0 +1,30 @@
+import os
+
+def get_this_dir():
+    return os.path.dirname( os.path.abspath(__file__) )
+
+
+def get_data_source():
+    return os.path.join(get_this_dir(), os.pardir, 'Data_Source')
+
+
+def get_raw_data():
+    return os.path.join(get_data_source(), 'Raw')
+
+def M0_6():
+    return os.path.join(get_raw_data(), 'M0_6')
+
+def M0_7():
+    return os.path.join(get_raw_data(), 'M0_7')
+
+def M0_8():
+    return os.path.join(get_raw_data(), 'M0_8')
+
+
+def get_result_data():
+    return os.path.join(get_data_source(), 'Results')
+
+def get_models():
+    return os.path.join(get_result_data(),'Models')
+
+
